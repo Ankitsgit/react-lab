a96233efd3bddd611e0e924a003ac3fc46a3010b
Ankitsgit <ankit77us@gmail.com>
2025-03-28 11:47:15 +0530

    todo with reduxToolkit is completed

--- 15reduxToolkitTodo/src/App.jsx
+++ 15reduxToolkitTodo/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todo from   './components/Todo'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-     <h1>Todo with reduxToolkit</h1>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo/>
+      <Todo/>
+
+
     </>
   )
 }
